a6dfebf93a1201519b1e8d1f8a3fbe2aeedefe6e
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2023-02-25 16:43:40 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,34 @@ import '../styles/Navbar.css';
 export const Navbar = () => {
   return (
     <div>
-        <div className='navbar'>Navbar</div>
+        <div className='navbar'>
+            <div className='logo_div'>
+                <img src="/Images/wstf_logo.svg" alt="" />
+                <h2>Wasserstoff</h2>
+            </div>
+            <div className='searchbar_div'>
+                <input type="text" />
+                <img src="/Images/search_icon.svg" alt="" />
+            </div>
+            <div className='redirects_div'>
+                <h3>
+                    <a className='link' href="">Statistics</a>
+                </h3>
+                <h3>
+                    <a className='link' href="">Overview</a>
+                </h3>
+                <h3>
+                    <a className='link' href="">Dashboard</a>
+                </h3>
+                <h3>
+                    <a className='link' href="">Analytics</a>
+                </h3>
+            </div>
+            <div className='details_div'>
+                <img src="/Images/user_icon.svg" alt="" />
+                <img src="/Images/settings_icon.svg" alt="" />
+            </div>
+        </div>
     </div>
   )
 }
