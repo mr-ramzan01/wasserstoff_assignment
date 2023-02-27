1e71bedecab650cd3afe2e3f86ff390d6203be3b
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2023-02-27 23:29:38 +0530

    overview static done

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import { Dashboard } from '../pages/Dashboard'
-import { Home } from '../pages/Home'
+import { Overview } from '../pages/Overview'
 
 export const AllRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Overview></Overview>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
     </Routes>
   )
