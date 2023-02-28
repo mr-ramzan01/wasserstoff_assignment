cc0f593911f591fc9fe6f1fc78c2553deb1c7b4e
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2023-03-01 04:45:13 +0530

    one bug fixed

--- src/components/LargeBubble.jsx
+++ src/components/LargeBubble.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import styles from '../styles/dashboard.module.css';
 
 export const LargeBubble = ({left, top}) => {
   return (
-    <div style={{left: left, top: top}} className='largeBubble'></div>
+    <div style={{left: left, top: top}} className={styles.largeBubble}></div>
   )
 }
